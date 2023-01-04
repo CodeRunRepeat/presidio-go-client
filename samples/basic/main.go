0484272c8280b10251b1c9b4a960a1deb437b0e5
Natasa Manousopoulou <natasa.manousopoulou@outlook.com>
2023-01-04 16:37:56 +0000

    Added sample

--- samples/basic/main.go
+++ samples/basic/main.go
@@ -8,7 +8,14 @@ import (
 
 func main() {
 	var baseUrl string = "https://presidio-analyzer-prod.azurewebsites.net"
-	var client = client.NewClient(baseUrl)
+
+	basicSample(baseUrl)
+
+	basicAuthSample(baseUrl)
+}
+
+func basicSample(baseUrl string) {
+	var client = client.NewClient(baseUrl, nil)
 	{
 		var result, err = client.Health()
 		fmt.Printf("Health() returned: %v, %v\n", result, err)
@@ -16,5 +23,14 @@ func main() {
 	{
 		var result, err = client.AnalyzeWithDefaults("My name is Joe", "en")
 		fmt.Printf("AnalyzeWithDefaults() returned: %v, %v\n", result, err)
+	}
+}
+
+func basicAuthSample(baseUrl string) {
+	var client = client.NewClient(baseUrl, client.BasicAuth{UserName: "test", Password: "pass@word1"}) /* imaginary credentials*/
+
+	{
+		var result, err = client.Health()
+		fmt.Printf("Health() returned: %v, %v\n", result, err)
 	}
 }
