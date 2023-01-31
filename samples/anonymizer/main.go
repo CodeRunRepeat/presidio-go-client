4ff14344d0b2185b3115479ed8ba60d207587a87
Natasa Manousopoulou <natasa.manousopoulou@outlook.com>
2023-01-31 11:33:18 +0000

    Added anonymizer samples, custom marshal

--- samples/anonymizer/main.go
+++ samples/anonymizer/main.go
@@ -6,16 +6,65 @@ import (
 	"github.com/CodeRunRepeat/presidio-go-client/client"
 )
 
+const (
+	ENCRYPTION_KEY = "AAECAwQFBgcICQoLDA0ODw=="
+	HASH_ALGORITHM = "sha256"
+)
+
 func main() {
-	//var analyzerBaseUrl string = "https://presidio-analyzer-prod.azurewebsites.net"
+	var analyzerBaseUrl string = "https://presidio-analyzer-prod.azurewebsites.net"
 	var anonymizerBaseUrl string = "https://presidio-anonymizer-prod.azurewebsites.net"
 
+	encryptSample(analyzerBaseUrl, anonymizerBaseUrl)
+	hashSample(analyzerBaseUrl, anonymizerBaseUrl)
+	maskSample(analyzerBaseUrl, anonymizerBaseUrl)
+	replaceSample(analyzerBaseUrl, anonymizerBaseUrl)
+	redactSample(analyzerBaseUrl, anonymizerBaseUrl)
 	presidioAsAnEncryptionService(anonymizerBaseUrl)
 	presidioAsAHashingService(anonymizerBaseUrl)
 }
 
-func presidioAsSomethingElse(analyzerBaseUrl string, anonymizer client.Anonymizer, description string) {
-	var c = client.NewClient(analyzerBaseUrl, nil)
+func encryptSample(analyzerBaseUrl string, anonymizerBaseUrl string) {
+	anonymizeSample(analyzerBaseUrl, anonymizerBaseUrl, client.EncryptAnonymizer{Key: ENCRYPTION_KEY})
+}
+
+func hashSample(analyzerBaseUrl string, anonymizerBaseUrl string) {
+	anonymizeSample(analyzerBaseUrl, anonymizerBaseUrl, client.HashAnonymizer{HashType: HASH_ALGORITHM})
+}
+
+func maskSample(analyzerBaseUrl string, anonymizerBaseUrl string) {
+	anonymizeSample(analyzerBaseUrl, anonymizerBaseUrl, client.MaskAnonymizer{MaskingChar: "*", CharsToMask: 8})
+}
+
+func redactSample(analyzerBaseUrl string, anonymizerBaseUrl string) {
+	anonymizeSample(analyzerBaseUrl, anonymizerBaseUrl, client.RedactAnonymizer{})
+}
+
+func replaceSample(analyzerBaseUrl string, anonymizerBaseUrl string) {
+	anonymizeSample(analyzerBaseUrl, anonymizerBaseUrl, client.ReplaceAnonymizer{NewValue: "<REMOVED>"})
+}
+
+func anonymizeSample(analyzerBaseUrl string, anonymizerBaseUrl string, anonymizer client.Anonymizer) {
+	var analyzerClient = client.NewClient(analyzerBaseUrl, nil)
+	const SENTENCE string = "On September 18 I visited microsoft.com and sent an email to test@presidio.site, from the IP 192.168.0.1"
+	analyzerResult, err := analyzerClient.AnalyzeWithDefaults(SENTENCE, "en")
+
+	if printError(err, "AnalyzeWithDefaults") {
+		return
+	}
+
+	var anonymizerClient = client.NewClient(anonymizerBaseUrl, nil)
+	anonymizers := make(client.AnonymizerSet)
+	anonymizers.AddDefaultAnonymizer(anonymizer)
+	anonymizedText, _, err := anonymizerClient.Anonymize(SENTENCE, &anonymizers, &analyzerResult)
+
+	if !printError(err, "Anonymize") {
+		fmt.Printf("Anonymized text: '%v'\n", anonymizedText)
+	}
+}
+
+func presidioAsSomethingElse(anonymizerBaseUrl string, anonymizer client.Anonymizer, description string) {
+	var c = client.NewClient(anonymizerBaseUrl, nil)
 
 	const SENTENCE string = "This is a test"
 	anonymizers := make(client.AnonymizerSet)
@@ -25,23 +74,24 @@ func presidioAsSomethingElse(analyzerBaseUrl string, anonymizer client.Anonymize
 	fakeResult.Matches[0] = client.AnalyzerMatch{Start: 0, End: int32(len(SENTENCE)), Score: 1.00, EntityType: "ANY"}
 
 	transformed, _, err := c.Anonymize(SENTENCE, &anonymizers, &fakeResult)
-	printError(err, "Anonymize")
 
-	if err == nil {
+	if !printError(err, "Anonymize") {
 		fmt.Printf("%v value of '%v' is '%v'\n", description, SENTENCE, transformed)
 	}
 }
 
-func presidioAsAHashingService(analyzerBaseUrl string) {
-	presidioAsSomethingElse(analyzerBaseUrl, client.HashAnonymizer{HashType: "sha256"}, "Hashed")
+func presidioAsAHashingService(anonymizerBaseUrl string) {
+	presidioAsSomethingElse(anonymizerBaseUrl, client.HashAnonymizer{HashType: HASH_ALGORITHM}, "Hashed")
 }
 
-func presidioAsAnEncryptionService(analyzerBaseUrl string) {
-	presidioAsSomethingElse(analyzerBaseUrl, client.EncryptAnonymizer{Key: "AAECAwQFBgcICQoLDA0ODw=="}, "Encrypted")
+func presidioAsAnEncryptionService(anonymizerBaseUrl string) {
+	presidioAsSomethingElse(anonymizerBaseUrl, client.EncryptAnonymizer{Key: ENCRYPTION_KEY}, "Encrypted")
 }
 
-func printError(err error, funcName string) {
+func printError(err error, funcName string) bool {
 	if err != nil {
 		fmt.Printf("Error in calling %v(): %v\n", funcName, err)
+		return true
 	}
+	return false
 }
